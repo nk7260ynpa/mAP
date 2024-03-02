ee764c57909c8985fd874b4a72360ccd89be5d63
nk7260ynpa <nk7260ynpa@gmail.com>
2024-03-02 22:32:25 +0800

    Add IoU calculate

--- Make_Short_instance.py
+++ Make_Short_instance.py
@@ -1,13 +1,10 @@
 import utils
-import random
 import argparse
-
-random.seed(1048596)
 
 def remove_images(gt, images_num_list) -> None:
     gt_images = []
     for img_info in gt["images"]:
-        if img_info["id"] in images_num_list:
+        if str(img_info["id"]) in images_num_list:
             gt_images.append(img_info)
     gt["images"] = gt_images
     return gt
@@ -15,7 +12,7 @@ def remove_images(gt, images_num_list) -> None:
 def remove_annotations(gt, images_num_list) -> None:
     gt_annotations = []
     for ann in gt["annotations"]:
-        if ann["image_id"] in images_num_list:
+        if str(ann["image_id"]) in images_num_list:
             ann['segmentation'] = []
             gt_annotations.append(ann)
     gt["annotations"] = gt_annotations
@@ -25,14 +22,14 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Make Short COCO Instance')
     parser.add_argument('--input', help='input json file', default='annotations/instances_val2014.json')
     parser.add_argument('--output', help='output json file', default='annotations/Short_instances.json')
+    parser.add_argument('--img_num', help='images num list', default="147293, 141108, 323202")
     return parser.parse_args()
 
 def main(opt) -> None:
     gt = utils.load_json(opt.input)
     # the 3 images are few annotations
-    images_num_list = [147293, 141108, 323202]
-    gt = remove_images(gt, images_num_list)
-    gt = remove_annotations(gt, images_num_list)
+    gt = remove_images(gt, opt.img_num.split(","))
+    gt = remove_annotations(gt, opt.img_num.split(","))
 
     utils.save_json(gt, opt.output)
 
--- tests/test_Make_Short_instance.py
+++ tests/test_Make_Short_instance.py
@@ -3,7 +3,7 @@ import Make_Short_instance
 
 def test_remove_images():
     gt = {"images": [{"id": 1}, {"id": 2}, {"id": 3}, {"id": 4}, {"id": 5}]}
-    images_num_list = [1, 2, 3]
+    images_num_list = ['1', '2', '3']
     result = Make_Short_instance.remove_images(gt, images_num_list)
     assert result == {"images": [{"id": 1}, {"id": 2}, {"id": 3}]}
 
@@ -14,7 +14,7 @@ def test_remove_annotations():
                           {"image_id": 3, "segmentation": [33.5, 22.4, 22.17, 12.67]}, 
                           {"image_id": 4, "segmentation": [33.5, 22.2, 22.13, 10.67]}, 
                           {"image_id": 5, "segmentation": [33.4, 22.4, 29.13, 12.67]}]}
-    images_num_list = [1, 2, 3]
+    images_num_list = ['1', '2', '3']
     result = Make_Short_instance.remove_annotations(gt, images_num_list)
     assert result == {"annotations": [{"image_id": 1, "segmentation":[]}, 
                                       {"image_id": 2, "segmentation":[]}, 
